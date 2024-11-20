dda8bdf0010c1f74d347dd5f7adfa8e9049fb44a
Martin Kapal <flamecze@gmail.com>
2024-11-20 19:37:07 +0100

    Turn signal checker concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,71 @@
-import { GlobalProviders } from "./GlobalProviders";
-import { ButtonsForEachConnection } from "@/components/ButtonsForEachConnection";
+import {
+  ConnectionsProvider,
+  PlayersProvider,
+  useOnPacket,
+  usePlayers,
+} from "react-node-insim";
 import { ConnectionButton } from "@/components/ConnectionButton";
+import { ButtonsForEachConnection } from "@/components/ButtonsForEachConnection";
+import { IS_MTC, PacketType } from "node-insim/packets";
+import { log } from "@/utils/log";
 
 export function App() {
   return (
-    <GlobalProviders>
-      <ButtonsForEachConnection>
-        <ConnectionButton
-          left={40}
-          top={40}
-          width={30}
-          height={8}
-          variant="dark"
-        >
-          test
-        </ConnectionButton>
-      </ButtonsForEachConnection>
-    </GlobalProviders>
+    <ConnectionsProvider>
+      <PlayersProvider>
+        <TurnSignalChecker />
+        <ButtonsForEachConnection>
+          <ConnectionButton
+            left={40}
+            top={40}
+            width={30}
+            height={8}
+            variant="dark"
+          >
+            test
+          </ConnectionButton>
+        </ButtonsForEachConnection>
+      </PlayersProvider>
+    </ConnectionsProvider>
   );
+}
+
+function TurnSignalChecker() {
+  const players = usePlayers();
+
+  useOnPacket(PacketType.ISP_III, (packet, inSim) => {
+    const args = packet.Msg.split(" ");
+
+    if (packet.Msg.startsWith("DL_SIGNAL_L")) {
+      const playerId = parseInt(args[1], 10);
+      const player = players.get(playerId);
+
+      if (!player) {
+        return;
+      }
+
+      inSim.send(
+        new IS_MTC({
+          UCID: 255,
+          Text: `${player.PName}^9 - Left signal ON`,
+        }),
+      );
+    } else if (packet.Msg.startsWith("DL_SIGNAL_OFF")) {
+      const playerId = parseInt(args[1], 10);
+      const player = players.get(playerId);
+
+      if (!player) {
+        return;
+      }
+
+      inSim.send(
+        new IS_MTC({
+          UCID: 255,
+          Text: `${player.PName}^9 - Left signal OFF`,
+        }),
+      );
+    }
+  });
+
+  return null;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,49 +1,34 @@
 import "./env";
-import {
-  IS_MTC,
-  IS_TINY,
-  MessageSound,
-  PacketType,
-  TinyType,
-} from "node-insim/packets";
+import { IS_MTC, MessageSound, PacketType } from "node-insim/packets";
 import { createRoot } from "react-node-insim";
-import * as trafficLights from "@/modules/trafficLights/trafficLights";
 import { App } from "./App";
 import { log } from "./utils/log";
 
-const root = createRoot({
+const { render, inSim } = createRoot({
   name: "Driving InSim",
   host: process.env.HOST ?? "127.0.0.1",
   port: process.env.PORT ? parseInt(process.env.PORT) : 29999,
   adminPassword: process.env.ADMIN ?? "",
 });
 
-root.render(<App />);
+render(<App />);
 
-root.inSim.on("connect", () => log("Connected"));
-root.inSim.on("disconnect", () => log("Disconnected"));
+inSim.on("connect", () => log("InSim connected"));
+inSim.on("disconnect", () => log("InSim disconnected"));
 
-root.inSim.on(PacketType.ISP_VER, (packet) => {
+inSim.on(PacketType.ISP_VER, (packet) => {
   log(`Connected to LFS ${packet.Product} ${packet.Version}`);
 
-  root.inSim.send(
+  inSim.send(
     new IS_MTC({
-      Text: "Driving InSim Connected",
+      Text: "Driving InSim connected",
       UCID: 255,
       Sound: MessageSound.SND_SYSMESSAGE,
     }),
   );
-
-  root.inSim.send(
-    new IS_TINY({
-      ReqI: 1,
-      SubT: TinyType.TINY_NPL,
-    }),
-  );
-
-  trafficLights.initialize(root.inSim);
 });
 
 process.on("uncaughtException", (error) => {
   log(error);
+  inSim.disconnect();
 });
